f794e773fc8e2656b9a825cc3a907de92d0930f6
rushi <rushiparikh6053@gmail.com>
2017-12-18 08:07:25 +0530

    Activity Detail

---
+++ app/src/main/java/com/example/rushi/recycleview/DbVisitContract.java
@@ -0,0 +1,21 @@
+package com.example.rushi.recycleview;
+
+/**
+ * Created by rushi on 12/16/2017.
+ */
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by dhaval on 06/12/2017.
+ */
+
+public class DbVisitContract implements BaseColumns {
+    public static final int DATABASE_VERSION =1;
+    public static final String DATABASE_NAME="entry.db";
+    public static final String TABLE_NAME = "entry";
+    public static final String COLUMN_NAME_TITLE = "title";
+
+
+}
+
--- app/src/main/java/com/example/rushi/recycleview/MainActivity.java
+++ app/src/main/java/com/example/rushi/recycleview/MainActivity.java
@@ -1,53 +1,79 @@
 package com.example.rushi.recycleview;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Handler;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView;
-    private Activity_DestinationAdapter adapter;
-    private List<Destination_Activity> imageList;
+    private static ViewPager mPager;
+    private static int currentPage = 0;
+    DbVisit  mDbHelper;
+    private static final Integer[] XMEN= {R.drawable.na1,R.drawable.na2,R.drawable.na3};
+    private ArrayList<Integer> XMENArray = new ArrayList<Integer>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView=findViewById(R.id.recycleview);
-        imageList=new ArrayList<>();
-        adapter=new Activity_DestinationAdapter(this,imageList);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(),LinearLayoutManager.VERTICAL,false));
-        recyclerView.setAdapter(adapter);
-        prepareImage();
+        mDbHelper = new DbVisit(this);
+
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+// Create a new map of values, where column names are the keys
+        ContentValues values = new ContentValues();
+        values.put(DbVisitContract.COLUMN_NAME_TITLE, "True");
 
 
+// Insert the new row, returning the primary key value of the new row
+        long newRowId = db.insert(DbVisitContract.TABLE_NAME, null, values);
+
+        if(newRowId > 0){
+            Log.e("DOOOO","DATA INSRETED");
+
+        }
+        db.close();
+        init();
     }
-    private void prepareImage() {
-        int[] covers = new int[]{
-                R.drawable.album1,
-                R.drawable.album2,
-                R.drawable.album3,
-                R.drawable.album4,
-                };
+    private void init() {
+        for(int i=0;i<XMEN.length;i++)
+            XMENArray.add(XMEN[i]);
 
-        Destination_Activity a = new Destination_Activity("True Romance", 13, covers[0]);
-        imageList.add(a);
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(new PageAdapter(MainActivity.this,XMENArray));
 
-        a = new Destination_Activity("Xscpae", 8, covers[1]);
-        imageList.add(a);
 
-        a = new Destination_Activity("Maroon 5", 11, covers[2]);
-        imageList.add(a);
 
-        a = new Destination_Activity("Born to Die", 12, covers[3]);
-        imageList.add(a);
-
-        adapter.notifyDataSetChanged();
+        // Auto start of viewpager
+        final Handler handler = new Handler();
+        final Runnable Update = new Runnable() {
+            public void run() {
+                if (currentPage == XMEN.length) {
+                    currentPage = 0;
+                }
+                mPager.setCurrentItem(currentPage++, true);
+            }
+        };
+        Timer swipeTimer = new Timer();
+        swipeTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(Update);
+            }
+        }, 2500, 2500);
     }
 }
